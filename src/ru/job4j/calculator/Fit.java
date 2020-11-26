56f4659e49f4fb1316fa024fe96dc5b519fb3223
k00lith <koolith@gmail.com>
2020-11-26 11:17:20 +0300

    9.1. String [#413387] fix variable

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -16,8 +16,8 @@ public class Fit {
         short height = 187;
         double man = Fit.manWeight(height);
         System.out.println("Man 187 is " + man);
-        double Woman = Fit.womanWeight(height);
-        System.out.println("Woman 187 is " + Woman);
+        double woman = Fit.womanWeight(height);
+        System.out.println("Woman 187 is " + woman);
     }
 
 }
