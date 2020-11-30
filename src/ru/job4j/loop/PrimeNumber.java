46a77f4964d4a0ccd9b316c8573fcea7a7c19352
k00lith <koolith@gmail.com>
2020-11-30 15:06:38 +0300

    5.6. Простые числа [#413435] удаление избыточной проверки

--- src/ru/job4j/loop/PrimeNumber.java
+++ src/ru/job4j/loop/PrimeNumber.java
@@ -1,15 +1,11 @@
 package ru.job4j.loop;
 
 public class PrimeNumber {
-
     public static int calc(int finish) {
         int count = 0;
-        if (finish > 1) {
-            for (int number = 2; number <= finish; number++) {
-                if (CheckPrimeNumber.check(number)) {
-                    count += 1;
-
-                }
+        for (int number = 2; number <= finish; number++) {
+            if (CheckPrimeNumber.check(number)) {
+                count += 1;
             }
         }
         return count;
