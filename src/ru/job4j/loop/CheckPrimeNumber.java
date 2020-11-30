bcba91a68312ac495792b61568794e5ce979347d
k00lith <koolith@gmail.com>
2020-11-30 14:57:38 +0300

    5.5. Простое число [#413437] изменение цикла

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,16 +2,13 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        //boolean prime = true;
-        boolean prime = number != 1 ? true : false;
-        for (int index = 2; index <= number - 1; index++) {
+        boolean prime = number > 1;
+        for (int index = 2; index < number; index++) {
             if (number % index == 0) {
                 prime = false;
                 break;
             }
-
         }
         return prime;
     }
 }
-
--- test/ru/job4j/loop/CheckPrimeNumberTest.java
+++ test/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -23,4 +23,10 @@ public class CheckPrimeNumberTest {
         boolean rsl = CheckPrimeNumber.check(1);
         assertThat(rsl, is(false));
     }
+
+    @Test
+    public void when25() {
+        boolean rsl = CheckPrimeNumber.check(25);
+        assertThat(rsl, is(false));
+    }
 }
