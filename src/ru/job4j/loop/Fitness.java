b2ea98730c0939f9ccad6e3d37ce5ef0ac6fb047
k00lith <koolith@gmail.com>
2020-11-30 14:11:11 +0300

    5.4. Протеиновая диета [#413434] Исправление условия

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -3,11 +3,10 @@ package ru.job4j.loop;
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
-        if (ivan <= nik) {
-            while (ivan < nik * 2) {
-                ivan *= 3;
-                month += 1;
-            }
+        while (ivan <= nik) {
+            ivan *= 3;
+            nik *= 2;
+            month += 1;
         }
         return month;
     }
@@ -19,4 +18,12 @@ Fitness.calc(50, 90);
 180
 Ivan *3
 Nik *2
+
+if (ivan <= nik) {
+эта проверка не нужна
+while (ivan < nik * 2) {
+тут лучше проще
+while (ivan =< nik) {
+а остальное в цикле так как Иван должен обогнать
+
  */
