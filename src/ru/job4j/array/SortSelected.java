ffcc417be5d3d3c0f46fbcbae3aefdad28a6440e
k00lith <koolith@gmail.com>
2020-12-03 17:36:55 +0300

    6.5.3. Сортировка выборкой. [#413352] Удалил избыточную проверку, изменил входные данные

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -4,7 +4,7 @@ public class SortSelected {
 
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
-            int min = MinDiapason.findMin(data, i, data.length);                  // этот метод нужен для получения минимально элемента в массиве
+            int min = MinDiapason.findMin(data, i, data.length - 1);                  // этот метод нужен для получения минимально элемента в массиве
             int index = FindLoop.indexOfStartFinish(data, min, i, data.length);   // этот метод нужен, чтобы получить индекс элемента, полученного из метода MinDiapason.findMin
             int temp = data[i];
             data[i] = data[index];
