1a3e76a08854f6eb3acd19574fd9919ca5610b19
k00lith <koolith@gmail.com>
2020-12-03 17:30:22 +0300

    6.5.2. Поиск минимума в диапазоне. [#413354] Удалил сравнение переменной с самой собой в начале цикла

--- src/ru/job4j/array/MinDiapason.java
+++ src/ru/job4j/array/MinDiapason.java
@@ -4,7 +4,7 @@ public class MinDiapason {
     public static int findMin(int[] array, int start, int finish) {
         int min = array[start];
         finish = finish == array.length ? finish - 1 : finish;
-        for (int i = start; i <= finish; i++) {
+        for (int i = start + 1; i <= finish; i++) {
             if (min > array[i]) {
                 min = array[i];
             }
