fe0ddcac4962f5aed7def550bf352c4c3d8fa594
k00lith <koolith@gmail.com>
2020-12-03 17:19:38 +0300

    6.4.2. Слово заканчивается на ... [#413350] добавление цикла для прохода по массиву

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -3,8 +3,14 @@ package ru.job4j.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
-        if (word[word.length - 1] != post[post.length - 1]) {
-            result = false;
+        int postIndex = 0;
+        for (int i = (word.length - post.length); i < word.length; i++) {
+            if (word[i] != post[postIndex]) {
+                result = false;
+                break;
+            } else {
+                postIndex++;
+            }
         }
         /* проверить. что массив word имеет последние элементы одинаковые с post */
         return result;
