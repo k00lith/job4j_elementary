c7f80980fb4fcda61c21e14d0b0be6fd70b842d2
k00lith <koolith@gmail.com>
2020-12-06 23:57:00 +0300

    4. Расстояние между точками в трехмерном пространстве. [#413535]

--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -1,51 +1,24 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
 import org.junit.Test;
+import static org.hamcrest.Matchers.closeTo;
+import static org.junit.Assert.assertThat;
 
 public class PointTest {
+
     @Test
-    public void when00to20then2() {
-        double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        //double out = Point.distance(x1, y1, x2, y2);
-        //Assert.assertEquals(expected, out, 0.01);
+    public void distance() {
+        Point a = new Point(1, 2);
+        Point b = new Point(3, 4);
+        double rsl = a.distance(b);
+        assertThat(rsl, closeTo(2.8, 0.03));
     }
 
     @Test
-    public void when02to20then2() {
-        double expected = 2.82;
-        int x1 = 0;
-        int y1 = 2;
-        int x2 = 2;
-        int y2 = 0;
-        //double out = Point.distance(x1, y1, x2, y2);
-        //Assert.assertEquals(expected, out, 0.01);
+    public void distance3d() {
+        Point a = new Point(1, 2, 5);
+        Point b = new Point(3, 4, 8);
+        double rsl = a.distance3d(b);
+        assertThat(rsl, closeTo(4.1, 0.1));
     }
-
-    @Test
-    public void when10to20then2() {
-        double expected = 2;
-        int x1 = 1;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        //double out = Point.distance(x1, y1, x2, y2);
-        //Assert.assertEquals(expected, out, 1.00);
-    }
-
-    @Test
-    public void when30to20then2() {
-        double expected = 2;
-        int x1 = 3;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        //double out = Point.distance(x1, y1, x2, y2);
-        //Assert.assertEquals(expected, out, 1.00);
-    }
-
 }
