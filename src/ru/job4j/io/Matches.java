58711902c343efa53c219cbf1abea12d41b793aa
k00lith <koolith@gmail.com>
2020-12-14 23:36:57 +0300

    2. Scanner и чтение числа из консоли. [#413487] Исправления: -- Установка явного условие выхода из цикла; -- Валидиация пользовательского ввода; -- Избавление от дублирования кода -- Вывод имя победителя после цикла

--- src/ru/job4j/io/Matches.java
+++ src/ru/job4j/io/Matches.java
@@ -3,28 +3,35 @@ package ru.job4j.io;
 import java.util.Scanner;
 
 public class Matches {
+
+    public static int move(String player) {
+        Scanner inputPlayer = new Scanner(System.in);
+        System.out.println(player + ", возьмите спички от 1 до 3 шт");
+        int selectPlayer = Integer.valueOf(inputPlayer.nextLine());
+        if (selectPlayer < 1 || selectPlayer > 3) {
+            System.out.println("Ошибка ввода, необходимо ввести число от 1 до 3 включительно. Переход хода");
+            return 0;
+        } else {
+            return selectPlayer;
+        }
+    }
+
     public static void main(String[] args) {
-        Scanner inputPlayer1 = new Scanner(System.in);
-        Scanner inputPlayer2 = new Scanner(System.in);
-        boolean run  = true;
+        boolean run = true;
         int matches = 11;
         while (run) {
-            System.out.println("Player1, возьмите спички от 1 до 3 шт");
-            int selectPlayer1 = Integer.valueOf(inputPlayer1.nextLine());
-            matches = matches - selectPlayer1;
-            System.out.println(matches);
-            if (matches == 0) {
-                run = false;
+            matches = matches - move("Player1");
+            System.out.println("Остаток " + matches);
+            if (matches <= 0) {
                 System.out.println("Player1 win!");
+                run = false;
                 break;
             }
-            System.out.println("Player2, возьмите спички от 1 до 3 шт");
-            int selectPlayer2 = Integer.valueOf(inputPlayer2.nextLine());
-            matches = matches - selectPlayer2;
-            System.out.println(matches);
-            if (matches == 0) {
-                run = false;
+            matches = matches - move("Player2");
+            System.out.println("Остаток " + matches);
+            if (matches <= 0) {
                 System.out.println("Player2 win!");
+                run = false;
                 break;
             }
         }
