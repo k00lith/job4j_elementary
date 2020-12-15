f2ea02a67317fcf8f3b3b9c90b79c8191f031c10
k00lith <koolith@gmail.com>
2020-12-15 14:45:37 +0300

    3.2. Зачем нужно приведение типов [#413496]

---
+++ src/ru/job4j/oop/Airplane.java
@@ -0,0 +1,7 @@
+package ru.job4j.oop;
+
+public class Airplane implements Vehicle {
+    public void move() {
+        System.out.println("Fly in air");
+    }
+}
---
+++ src/ru/job4j/oop/Train.java
@@ -0,0 +1,7 @@
+package ru.job4j.oop;
+
+public class Train implements Vehicle {
+    public void move() {
+        System.out.println("Move on rails");
+    }
+}
