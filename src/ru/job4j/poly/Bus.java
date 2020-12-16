7b25bd2536127fa4b4d36e53fddbda94fc66781c
k00lith <koolith@gmail.com>
2020-12-16 21:37:20 +0300

    3. Интерфейс Input [#413488] Минимальное заполнение методов

--- src/ru/job4j/poly/Bus.java
+++ src/ru/job4j/poly/Bus.java
@@ -3,12 +3,12 @@ package ru.job4j.poly;
 public class Bus implements Transport {
     @Override
     public void move() {
-
+        System.out.println("Go");
     }
 
     @Override
     public void passengers(int quantity) {
-
+        System.out.println("In-Out");
     }
 
     @Override
